7946bf4b82b1a6c89308cf758fe37f1237934eab
Gabriel Damalis <gabrieldamalis@gmail.com>
2025-12-01 01:44:09 -0300

    fix: unexpected refreshed when changing language

--- src/components/shared/language-switcher/LanguageSwitcher.tsx
+++ src/components/shared/language-switcher/LanguageSwitcher.tsx
@@ -3,8 +3,8 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import { ChevronDownIcon, GlobeAltIcon } from "@heroicons/react/24/outline";
 import { i18n, type Locale } from "@src/i18n/config";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { Link, usePathname } from "@src/i18n/routing";
+import { useLocale } from "next-intl";
 import { cn } from "@src/utils/cn";
 
 type LanguageSwitcherProps = {
@@ -15,14 +15,7 @@ export default function LanguageSwitcher({
   isScrolled = false,
 }: LanguageSwitcherProps) {
   const pathname = usePathname();
-  const currentLocale = pathname?.split("/")[1];
-
-  const redirectedPathname = (locale: Locale) => {
-    if (!pathname) return "/";
-    const segments = pathname.split("/");
-    segments[1] = locale;
-    return segments.join("/");
-  };
+  const currentLocale = useLocale();
 
   return (
     <Menu as="div" className="relative inline-block text-left">
@@ -57,8 +50,10 @@ export default function LanguageSwitcher({
                 <li key={locale}>
                   <MenuItem>
                     <Link
-                      href={redirectedPathname(locale)}
-                      className="block px-4 py-2 text-sm text-gray-700 dark:text-white data-[focus]:rounded-md data-[focus]:bg-gray-100 dark:data-[focus]:bg-gray-700 dark:data-[focus]:text-white data-[focus]:text-gray-900 data-[focus]:outline-none"
+                      href={pathname}
+                      locale={locale as Locale}
+                      scroll={false}
+                      className="w-full text-left block px-4 py-2 text-sm text-gray-700 dark:text-white data-[focus]:rounded-md data-[focus]:bg-gray-100 dark:data-[focus]:bg-gray-700 dark:data-[focus]:text-white data-[focus]:text-gray-900 data-[focus]:outline-none"
                     >
                       {locale?.split("-")[0]?.toUpperCase()}
                     </Link>
